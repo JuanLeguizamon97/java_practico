72160c9c978bdefabf6064dd19117bd9fd91e774
Juan Leguizamon <juanc.leguizamon@urosario.edu.co>
2022-07-19 09:11:26 -0500

    Manipulando el DOM con javascript

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const h1 = document.querySelector('h1');
 const p = document.querySelector('p');
-const parrafito = document.getElementsByClassName('.parrafito');
-const pid = document.getElementById('#pid');
+const parrafito = document.getElementsByClassName('parrafito');
+const pid = document.getElementById('pid');
 const input = document.querySelector('input');
 
 console.log({
@@ -11,3 +11,22 @@ console.log({
     pid,
     input,
 });
+
+
+h1.innerHTML = 'Patito <br> Feo';
+h1.innerText = "Patito 1";
+
+//console.log(h1.getAttribute('pantalla'));
+//h1.setAttribute('class' ,"rojo");
+
+h1.classList.add('rojo');
+h1.classList.remove('verde');
+//h1.classList.toogle('verde');
+//h1.classList.contains('verde');
+
+input.value = 456;
+
+const img = document.createElement('img');
+img.setAttribute('src', 'https://images.pexels.com/photos/12879613/pexels-photo-12879613.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1');
+
+pid.append(img);
